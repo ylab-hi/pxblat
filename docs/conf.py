34d98dd23c33bbc14c5e4ec4d9d478029b8ff9ab
Yangyang Li <yangyang.li@northwestern.edu>
2023-05-25 03:58:56 -0500

    Update version and dependencies for pxblat to 0.1.3

---
+++ docs/conf.py
@@ -0,0 +1,57 @@
+"""Sphinx configuration."""
+from datetime import datetime
+
+project = "pxblat"
+author = "Yangyang Li"
+copyright = f"{datetime.now().year}, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_click",
+    "myst_parser",
+]
+source_suffix = [".rst", ".md"]
+autodoc_typehints = "description"
+html_theme = "sphinx_material"
+html_logo = "_static/logo.png"
+
+myst_heading_anchors = 3
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+    "deflist",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "colon_fence",
+    "smartquotes",
+    "replacements",
+    "linkify",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the navigation.
+    "nav_title": "Project Name",
+    # Set you GA account ID to enable tracking
+    "google_analytics_account": "UA-XXXXX",
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    "base_url": "https://project.github.io/project",
+    # Set the color and the accent color
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    # Set the repo location to get a badge with stats
+    "repo_url": "https://github.com/project/project/",
+    "repo_name": "Project",
+    # Visible levels of the global TOC; -1 means unlimited
+    "globaltoc_depth": 3,
+    # If False, expand all TOC entries
+    "globaltoc_collapse": False,
+    # If True, show hidden TOC entries
+    "globaltoc_includehidden": False,
+}
