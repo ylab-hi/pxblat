277387084317d005754cdde73c3bb8b84f4d30b0
Yangyang Li <yangyang.li@northwestern.edu>
2023-06-06 02:12:08 -0500

    feat(cli): add support for twobittofa command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,9 +8,34 @@ from typer.testing import CliRunner
 runner = CliRunner()
 
 
+def read_fa_seq(file):
+    res = ""
+    with open(file) as f:
+        for line in f:
+            if line.startswith(">"):
+                continue
+            else:
+                res += line.strip()
+    return res
+
+
+def compare_two_fas(file1, file2):
+    file1_seq = read_fa_seq(file1)
+    file2_seq = read_fa_seq(file2)
+    assert file1_seq == file2_seq
+
+
 def test_call():
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
+
+
+def test_twobit2fa_cli(tmp_path, reference, two_bit):
+    out = tmp_path / "testcli.fa"
+    result = runner.invoke(app, ["twobittofa", two_bit.as_posix(), out.as_posix()])
+    assert result.exit_code == 0
+    assert out.exists()
+    compare_two_fas(reference, out)
 
 
 def test_fa2twobit_cli(tmp_path, two_bit):
