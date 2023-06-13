58b09aafe78bb9e661e744d79852702fe9f8c0d2
Yangyang Li <yangyang.li@northwestern.edu>
2023-06-13 14:11:33 -0500

    chore(tests): remove obsolete test_pickle file

--- tests/test_pickle.py
+++
@@ -1,9 +0,0 @@
-from pxblat.extc import ClientOption
-from pxblat.extc import set_state
-
-
-def test_set_state_for_client():
-    client_option = ClientOption().withHost("localhost2").build()
-    new_client_option = ClientOption()
-    set_state(new_client_option, client_option.__getstate__())
-    assert new_client_option.hostName == client_option.hostName
