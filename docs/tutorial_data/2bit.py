e3c4650ae833085d26e3d931369e2f60b585efab
Yangyang Li <yangyang.li@northwestern.edu>
2023-10-25 03:20:21 -0500

    docs: rewrite toturial

---
+++ docs/tutorial_data/2bit.py
@@ -0,0 +1,10 @@
+from pxblat import fa_to_two_bit
+
+fa_to_two_bit(
+    ["test_ref.fa"],
+    "test_ref.2bit",
+    noMask=False,
+    stripVersion=False,
+    ignoreDups=False,
+    useLong=False,
+)
