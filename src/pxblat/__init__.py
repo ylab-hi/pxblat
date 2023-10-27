baff89085415fb43e4cbe5f7c1e076ed2fffa1bc
Yangyang Li <yangyang.li@northwestern.edu>
2023-10-27 01:17:15 -0500

    chore: update version to 1.1.1

--- src/pxblat/__init__.py
+++ src/pxblat/__init__.py
@@ -26,7 +26,7 @@ from .toolkit import fa_to_two_bit
 from .toolkit import two_bit_to_fa
 from rich.traceback import install
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 install(show_locals=True)
 
