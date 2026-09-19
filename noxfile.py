aa45f1fdfee3a110fb54db3828f42a6eeef509ce
Yangyang Li <yangyang.li@northwestern.edu>
2026-09-18 23:58:38 -0500

    ci: fix mypy nox typing and pip constraint on py3.9 (diagnostic verbose install)

--- noxfile.py
+++ noxfile.py
@@ -23,13 +23,13 @@ except ImportError:
 package = "pxblat"
 python_versions = ["3.11", "3.10", "3.9"]
 nox.needs_version = ">= 2021.6.6"
-nox.options.sessions = (
+nox.options.sessions = [
     "pre-commit",
     "mypy",
     "tests",
     "docs-build",
-)
-DEBUG = False
+]
+DEBUG = True
 
 
 def poetry_install():
